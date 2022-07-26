5d5235a093ae51592d3866dc23c49adb8617a470
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-07-26 10:03:08 +0300

    Added functions

--- src/components/AddNotes.js
+++ src/components/AddNotes.js
@@ -1,1 +1,11 @@
-import React from "react";
+import React , {useState} from 'react'
+ function AddNote ({handleAddNote}) {
+    const [noteText , setNoteText] = useState('')
+    const charLimit = 300;
+    const handleChange =( event )=>{
+        if (charLimit - event.target.value.lengh>=0){
+            setNoteText(event.target.value)
+        }
+    
+ }
+ }
