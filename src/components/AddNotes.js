bee44abf6ba6e1a58d7dfd55383212c2f78d4d78
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-07-27 09:43:07 +0300

    Finished writting code

--- src/components/AddNotes.js
+++ src/components/AddNotes.js
@@ -1,11 +1,19 @@
-import React , {useState} from 'react'
- function AddNote ({handleAddNote}) {
-    const [noteText , setNoteText] = useState('')
+import React,{useState} from 'react'
+
+function AddNote({handleAddNote}) {
+    const [noteText,setNoteText] = useState('');
     const charLimit = 300;
-    const handleChange =( event )=>{
-        if (charLimit - event.target.value.lengh>=0){
-            setNoteText(event.target.value)
-        }
+    const handleChange =(event)=>{
+    if(charLimit - event.target.value.length>=0){
+        setNoteText(event.target.value)
+    }
+}
+   
+    }
     
- }
- }
+    const handleSaveClick =() =>{
+        if(noteText.trim().length>0){
+            handleAddNote(noteText);
+            setNoteText('');
+        }
+	}
